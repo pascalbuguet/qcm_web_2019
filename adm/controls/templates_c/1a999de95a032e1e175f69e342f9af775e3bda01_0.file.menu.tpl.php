4daa09f097e2c2c2a5fab21a4d16584629ecb54f
pascalbuguet <pascal.buguet@laposte.net>
2019-02-28 20:13:16 +0100

    Add files via upload

---
+++ adm/controls/templates_c/1a999de95a032e1e175f69e342f9af775e3bda01_0.file.menu.tpl.php
@@ -0,0 +1,41 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-02-28 19:57:53
+  from 'C:\xampp\htdocs\QCM_web_2019\adm\boundaries\menu.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c782f31de34b6_64770338',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '1a999de95a032e1e175f69e342f9af775e3bda01' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\QCM_web_2019\\adm\\boundaries\\menu.tpl',
+      1 => 1551376959,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c782f31de34b6_64770338 (Smarty_Internal_Template $_smarty_tpl) {
+?><nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+
+            <a class="navbar-brand" href="../controls/txt_2_bd_ctrl.php">TXT 2 BD</a>
+            <a class="navbar-brand" href="../controls/bd_2_txt_ctrl.php">BD 2 TXT</a>
+            <a class="navbar-brand" href="../controls/new_qcm_ctrl.php">New QCM</a>
+            <a class="navbar-brand" href="../controls/new_question_ctrl.php">New question</a>
+            <a class="navbar-brand" href="../controls/delete_question_ctrl.php">DELETE question</a>
+            <a class="navbar-brand" href="../controls/update_question_ctrl.php">UPDATE question</a>
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+<?php }
+}
